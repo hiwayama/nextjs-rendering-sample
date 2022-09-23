36e60dfa975bea47f75487d11334bddc394470c2
H.IWAYAMA <1152796+hiwayama@users.noreply.github.com>
2022-09-24 01:01:19 +0900

    update libs

--- src/pages/ssr/index.jsx
+++ src/pages/ssr/index.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import initValue from '../../../seed';
 
 export default function SSR(props) {
-  const [val, setVal] = useState(initValue());
+  const [val, setVal] = useState(0);
+  useEffect(() => {
+    setVal(initValue());
+  });
   const increment = () => {
     setVal(val + 1);
   };
